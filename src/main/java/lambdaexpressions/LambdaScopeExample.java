702f4e8fca9baa296500b8e1f1071631186b75b0
Filmon Tewelde <philmonephraim@gmail.com>
2024-06-29 12:00:38 +0400

    I fixed some errors and bugs in the code

--- src/main/java/lambdaexpressions/LambdaScopeExample.java
+++ src/main/java/lambdaexpressions/LambdaScopeExample.java
@@ -3,7 +3,7 @@ package lambdaexpressions;
 public class LambdaScopeExample {
     public static void main(String[] args) {
         int localVar = 6;
-        Calculator calculator = (a, b) -> a * b + localVar;
+        calculator calculator = (a, b) -> a * b + localVar;
         int result = calculator.calculate(3, 5);
         System.out.println(result);
     }
